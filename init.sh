93af88691da64df201e6486a7d1cabe32d1478ae
Taro Aoki <aizu.s1230022@gmail.com>
2016-04-01 20:59:29 +0900

    [Fix] Clone repository when $DOTPATH not exist

--- init.sh
+++ init.sh
@@ -5,20 +5,22 @@ RIPOSITORY_URL="https://github.com/lycoris0731/dotfiles"
 
 ROOT=$(cd $(dirname $0);pwd)
 
-# clone repository
-which git > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  git clone --recursive "$RIPOSITORY_URL" "$DOTPATH"
+if [ ! -e $DOTPATH ]; then
+  # clone repository
+  which git > /dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    git clone --recursive "$RIPOSITORY_URL" "$DOTPATH"
 
-else
-  echo "Please install git!"
-  exit 1
+  else
+    echo "Please install git!"
+    exit 1
+  fi
 fi
 
 # make synbolic links
-for f in .??*
-do
-  [ "$f" = ".git" ] && continue
+#for f in .??*
+#do
+#  [ "$f" = ".git" ] && continue
 
-  ln -sf "$ROOT"/dotfiles-master/"$f" "$HOME"/"$f"
-done
+#  ln -sf "$ROOT"/dotfiles-master/"$f" "$HOME"/"$f"
+#done
