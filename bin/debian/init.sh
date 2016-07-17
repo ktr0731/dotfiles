9a772cc7e18176d1930a595aff551a301b01f42e
Taro Aoki <aizu.s1230022@gmail.com>
2016-07-18 00:03:58 +0900

    [WIP] Add debian bootstrap script

--- bin/debian/init.sh
+++ bin/debian/init.sh
@@ -1,14 +1,19 @@
 #! /bin/sh
 
 # Change directories name to English
-# env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
-#
-# yes | sudo apt update
-# yes | sudo apt upgrade
+env LANGUAGE=C LC_MESSAGES=C xdg-user-dirs-gtk-update
+
+yes | sudo apt update
+yes | sudo apt upgrade
 
 # Install
-packages="strace ltrace"
+lib_packages="libgtk2.0-0:i386 libxxf86vm1:i386 libsm6:i386 lib32stdc++6 build-essential gcc-multilib"
+ctf_packages="gdb binutils strace ltrace"
 
-for package in $packages; do
-  echo $package
+for package in $lib_packages; do
+  sudo apt install "$package"
+done
+
+for package in $ctf_packages; do
+  sudo apt install "$package"
 done
