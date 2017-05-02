fd287086d435c9a41d7e56febbd0e04f4c34c7c7
lycoris0731 <aizu.s1230022@gmail.com>
2017-05-03 00:21:57 +0900

    [ADD] gini command

--- fish/config.fish
+++ fish/config.fish
@@ -64,7 +64,7 @@ function fish_right_prompt
 end
 
 function cd
-  if [ (count $argv) -ne 0 ]
+  if count $argv > /dev/null
     builtin cd $argv
   else
     z -l | awk '{ print $2 }' | fzy | read -l p
@@ -73,4 +73,29 @@ function cd
 
   ls
   echo ''
+end
+
+# gini [--gh repo_name]
+# gini does initial commit after initialize current directory
+function gini
+  touch README.md
+
+  git init
+  git add .
+  git commit -m '[ADD] Initial commit'
+
+  if count $argv > /dev/null
+    switch $argv[1]
+    case "--gh"
+      if [ (count $argv) -eq 2 ]
+        set REPO_NAME $argv[2]
+      else
+        set REPO_NAME (basename (pwd))
+      end
+      hub create -p $REPO_NAME
+    case '*'
+      echo 'Usage: dotc commit_msg --gh repo_name'
+      return
+    end
+  end
 end
