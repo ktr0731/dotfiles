80ddf7c5ab31dab12b50e692d679a8d99f9e53be
lycoris0731 <aizu.s1230022@gmail.com>
2017-06-24 14:55:38 +0900

    Add tmux config

--- xdg-config/fish/config.fish
+++ xdg-config/fish/config.fish
@@ -135,3 +135,19 @@ function gini
     end
   end
 end
+
+function t
+  tmux new-window -n (basename (pwd))
+end
+
+function select-window
+  if [ -z "$TMUX" ]
+    echo 'this session is out of tmux'
+    return 1
+  end
+
+  tmux list-window -F '#{window_id}: #{window_name}' | fzf | read -l p
+  tmux select-window -t (echo $p | cut -d : -f 1)
+end
+
+bind \cs select-window
